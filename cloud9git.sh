882cb5e9e640b0697e02968b5096aaae49359915
darko-mesaros <darko.subotica@gmail.com>
2019-09-17 09:27:23 +0300

    Modified Readme

--- cloud9git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "Darko Meszaros"
-git config --global user.email dmeszaro@amazon.de
-
-git config --global credential.helper '!aws codecommit credential-helper $@'
-git config --global credential.UseHttpPath true
